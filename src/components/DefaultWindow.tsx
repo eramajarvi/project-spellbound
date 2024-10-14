220d0b81022373ac4cd314f05730608f616e9bf0
James <danjame@hotmail.com>
2024-10-14 08:03:52 -0500

    improved default window design

--- src/components/DefaultWindow.tsx
+++ src/components/DefaultWindow.tsx
@@ -12,10 +12,13 @@ function DefaultWindow() {
       nodeRef={nodeRef}
       positionOffset={{ x: "0%", y: "20%" }}
     >
-      <div ref={nodeRef} className="window" style={{ width: "300px" }}>
+      <div ref={nodeRef} className="window" style={{ width: "400px" }}>
         <div className="title-bar" id="credits-window-title-bar">
           <div className="title-bar-text">Hola mundo</div>
           <div className="title-bar-controls">
+            <button aria-label="Help" />
+            <button aria-label="Minimize" />
+            <button aria-label="Maximize" />
             <button aria-label="Close" />
           </div>
         </div>
