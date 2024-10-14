b64b5d1464d1b0ea71dd88eb497e4654252f28c2
James <danjame@hotmail.com>
2024-10-14 06:59:52 -0500

    imported the styled window onto the desktop

--- src/components/Desktop.tsx
+++ src/components/Desktop.tsx
@@ -1,9 +1,16 @@
 import "xp.css/dist/XP.css";
 
+import BlissWallpaper from "../assets/Bliss.jpg";
+
+import React, { useState } from "react";
+import DefaultWindow from "./DefaultWindow";
+
 export default function Desktop() {
   return (
     <>
-      <div> Hola desde el escritorio</div>
+      <div className="crt"></div>
+      <div className="absolute h-full w-full bg-[url('src/assets/Bliss.jpg')] bg-no-repeat bg-cover"></div>
+      <DefaultWindow />
     </>
   );
 }
