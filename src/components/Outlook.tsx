66279a536afc73aaea238022d958949a3f5cf97a
James <danjame@hotmail.com>
2024-10-14 10:58:35 -0500

    improved outlook size

--- src/components/Outlook.tsx
+++ src/components/Outlook.tsx
@@ -11,6 +11,8 @@ import DeletedIcon from "../assets/deleted.png";
 import SentIcon from "../assets/sent.png";
 import PeopleIcon from "../assets/useremail.png";
 
+import Email01 from "./emails/Email-01";
+
 function OutlookWindow() {
   const nodeRef = React.useRef(null);
 
@@ -20,7 +22,11 @@ function OutlookWindow() {
       nodeRef={nodeRef}
       positionOffset={{ x: "0%", y: "20%" }}
     >
-      <div ref={nodeRef} className="window" style={{ width: "850px" }}>
+      <div
+        ref={nodeRef}
+        className="window"
+        style={{ width: "850px", height: "700px" }}
+      >
         <div className="title-bar" id="outlook-window-title-bar">
           <div className="title-bar-text flex items-center">
             <img src={OutlookIcon.src} />
@@ -196,8 +202,8 @@ function OutlookWindow() {
             </ul>
           </div>
 
-          <div className="w-[380px] ml-2 border-blue-400 border-4">
-            Hola mundo
+          <div className="w-[380px] ml-2 ">
+            <Email01 />
           </div>
         </div>
       </div>
