38fe9cc836600e2662e2b1890435fbb56144e320
James <danjame@hotmail.com>
2024-10-22 02:03:47 -0500

    Removed comment

--- src/components/Sopor.tsx
+++ src/components/Sopor.tsx
@@ -16,14 +16,6 @@ function Sopor({ soporVisibility, desktopVisibility }) {
     <>
       <div ref={nodeRef} className="flex relative fileExplorer">
         <img src={soporImage.src} />
-        {/* <a
-          className="absolute fileLink left-[300px] top-[100px] text-white text-2xl"
-          onClick={() => {
-            setSoporVisible(false), setDesktopVisible(true);
-          }}
-        >
-          entrar al sistema
-        </a> */}
       </div>
 
       <Draggable
