75ef2f6ec2e03cc51e804949f5b01e04281c44ac
James <danjame@hotmail.com>
2024-10-24 21:32:08 -0500

    Added all 10 email views

---
+++ src/components/emails/Email-02.tsx
@@ -0,0 +1,28 @@
+import PeopleIcon from "../../assets/useremail.png";
+
+export default function Email02() {
+  return (
+    <>
+      <div className="overflow-y-auto h-[610px] p-1 bg-white">
+        <div className="flex items-center bg-slate-100">
+          <div className="w-32">
+            <img src={PeopleIcon.src} className="size-fit" />
+          </div>
+          <div className="p-1">
+            <p className="font-bold text-lg leading-5">[URGENTE] Avistamientos detectados cerca de su área</p>
+
+            <p className="font-mono">notifications@fbi.gov</p>
+
+            <p className="font-extrabold">Recibido: justo ahora</p>
+          </div>
+        </div>
+
+        <div className="p-1">
+          <div className="text-sm">
+            Email02. Espere más instrucciones de nuestra parte una vez nos aseguremos que está bien.
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
